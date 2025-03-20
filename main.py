22f92b33dec0f74675ab2f2b49bbd63cc39c0abc
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-20 12:41:51 -0700

    contained password generator into a for loop

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import requests
 
 # 19 long password: ***-***-***-***-***
 
-randWord = RandomWords()
+password = ""
 
-print(randWord.get_random_word())
+for i in range(9):
+  randWord = RandomWords()
+  randNums = random.randint(100,999)
+  if i % 3 == 0:
+    password += randWord.get_random_word()
+    password += "-"
+  elif i % 2 == 0:
+    password += str(randNums)
+    password += "-"
+print(password)
 
 # print(random.random() * 10)
 
