4323ca4ee0cdfb648aa07eed8211193c5087a66b
Matthew Garcia <gmatthew2765@gmail.com>
2025-03-20 12:41:51 -0700

    implemented get

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import random
+import requests
 
 # 19 long password: ***-***-***-***-***
+
+response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+
+data = response.json()
+
+word = data[0]["word"]
+
+print(word)
 
 print(random.random() * 10)
 
